93795c8ba1fec6654c9d38df74fb9ef0bbb19aa9
chenzq <1127074163@qq.com>
2018-11-07 11:06:30 +0800

    sc

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+
 import xinx from '@/pages/xinx.vue'
+
 import px from '@/pages/px/px.vue'
 import pxbaomingxinxi from '@/pages/px/pxbaomingxinxi.vue'
 import pxmonikaoshi from '@/pages/px/pxmonikaoshi.vue'
 import pxmonikaosi from '@/pages/px/pxmonikaosi.vue'
 import pxxiangqing from '@/pages/px/pxxiangqing.vue'
+
+
 import zpgangwei from '@/pages/zp/zpgangwei'
 import zpgongzhuyoulun from '@/pages/zp/zpgongzhuyoulun.vue'
 import zpgoutomgliaojie from '@/pages/zp/zpgoutomgliaojie.vue'
@@ -15,6 +19,12 @@ import zpshouye from '@/pages/zp/zpshouye.vue'
 import zpzhaopinxiangqing from '@/pages/zp/zpzhaopinxiangqing.vue'
 import zpjieshaotanchuk from '@/pages/zp/zpjieshaotanchuk.vue'
 
+
+import sc from '@/pages/sc/sc.vue'
+import scdzht from '@/pages/sc/scdzht.vue'
+import schyjkz from '@/pages/sc/schyjkz.vue'
+import schyz from '@/pages/sc/schyz.vue'
+import scwz from '@/pages/sc/scwz.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -75,6 +85,26 @@ export default new Router({
       path: '/zpjieshaotanchuk',
       name: 'zpjieshaotanchuk',
       component: zpzhaopinxiangqing
+    },{
+      path: '/sc',
+      name: 'sc',
+      component: sc
+    },{
+      path: '/scdzht',
+      name: 'scdzht',
+      component: scdzht
+    },{
+      path: '/schyjkz',
+      name: 'schyjkz',
+      component: schyjkz
+    },{
+      path: '/schyz',
+      name: 'schyz',
+      component: schyz
+    },{
+      path: '/scwz',
+      name: 'scwz',
+      component: scwz
     }
   ]
 })
